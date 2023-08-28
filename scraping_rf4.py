9a9686dbffffb417968feb76b0ade1c924a4e935
SQL <14tytan14@gmail.com>
2023-08-28 22:02:36 +0200

    Update scraping_rf4.py

--- scraping_rf4.py
+++ scraping_rf4.py
@@ -35,6 +35,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 import datetime
 import time
+import re
 
 mydb = mysql.connector.connect(
     host = "localhost",
@@ -70,67 +71,118 @@ strony = [
 
 baits = []
 data = []
+wpis_problem = ''
 for a in range (0,len(strony)):
+#for a in range(3,4):
     driver.get(strony[a][0])
     time.sleep(0.2)
     website = (strony[a][1])
     cat = (strony[a][2])
     reg = (strony[a][3])
     bait = driver.find_elements(By.CLASS_NAME,"bait_icon")
-    for element in bait:        
+    for element in bait:
         bait = element.get_attribute("title")
         baits.append(bait)
-        
+
     wpisy = driver.find_elements(By.CLASS_NAME,"overflow ")
     menu_boczne = 5
     ilosc_wpisy = len(wpisy)-menu_boczne
-    i =-1     
 
-    for b in range(6,ilosc_wpisy):        
-    #for b in range(6,30):      #dev   
-        if (b % 6) == 0:
+    for b in range(6,ilosc_wpisy-1,6):
+    #for b in range(6,1800,6):
+        try:
             wpisy[b].click()
-            time.sleep(0.05)
-            i+=1
+            time.sleep(0.10)
 
-            #print(wpisy[b].text,'LEN:',len(wpisy[b].text))
-            if len(wpisy[b].text) == 0:
-                data.append(ryba)
-                #print('ryba dodana')
-                continue
+            data_small = []
+            ab = re.findall('[0-9]+',wpisy[b].text)
+            if ((wpisy[b+1].text).find(" kg") > 0 or (wpisy[b+1].text).find(" g") >0) and (len(ab)==0)  :
+
+                if len(wpisy[b].text) == 0:
+                    data_small.append(ryba)
+                else:
+                    data_small.append(wpisy[b].text)
+                    ryba = wpisy[b].text
+
+                data_small.append(wpisy[b+1].text)
+                data_small.append(wpisy[b+2].text)
+                data_small.append(wpisy[b+4].text)
+                data_small.append(wpisy[b+5].text)
             else:
-                data.append(wpisy[b].text)
-                ryba = wpisy[b].text
-                continue
-        
-            
-            
-        if ((b-3) % 6) == 0:
-            #print(baits[i]) #dev
-            data.append(baits[i])
-        else: 
-            #print(wpisy[b].text) #dev
-            data.append(wpisy[b].text)
-        
-    
-        if b == ilosc_wpisy-1:
-            break
-        
-    
 
-    for a in range (0,len(data),6):
-        if (a%6)==0 :
-            dzisiaj = data[a+5]
-            dzisiaj = dzisiaj.replace('.','-')
-            dzisiaj = dzisiaj[:-2] + '2023'
-            date_object = datetime.datetime.strptime(dzisiaj, '%d-%m-%Y').date()
-            date_object = date_object.strftime('%Y-%m-%d')
-            #print(date_object)
+
+                data_small = []
+                for problem in range (b,b+6):
+                    #print('problem data',problem,wpisy[problem].text,wpisy[problem-1].text,wpisy[problem+1].text,)
+
+                    if (wpisy[problem].text).find(" kg") > 0 or  (wpisy[problem].text).find(" g")   > 0  :
+
+                       if len(wpisy[problem-1].text) == 0:
+                            data_small.append(ryba)
+                       else:
+                           if len(re.findall('[0-9]+',wpisy[problem-1].text  ))==0:
+                               data_small.append(wpisy[problem-1].text)
+                               ryba = wpisy[problem-1].text
+
+
+                       data_small.append(wpisy[problem].text)
+                       data_small.append(wpisy[problem+1].text)
+                       data_small.append(wpisy[problem+3].text)
+                       data_small.append(wpisy[problem+4].text)
+                       #print('  dodalismy z bledu',data_small)
+                       break
+                    else:
+                       #print('  dalej')
+                       continue
+
+
+
+
+            #print(data_small) #dev
+            #time.slepp(10) #devc
+            #print(len(data_small))
+            if len(data_small) ==5 and len(re.findall('[0-9]+',data_small[0])  )==0:
+                data.append(data_small)
+        except Exception as e:
+            print (e)
+            print('problem krytyczny z ', b, wpisy[b].text )
+
+
+    # for datas in data:
+    #     print (datas)
+    #print(dupa)
+    #print(data)
+    complete_data = []
+    i = 0
+    for row in data:
+        if len(row[0]) ==0 or row[0] =='':
+            print('brak')
+        else:
+            #print([row,baits[i]],i) #dev
+            complete_data.append([row[0],row[1],row[2],row[3],row[4],baits[i]])
+        i+=1
+    for a in range (0,len(complete_data)-2):
+        dzisiaj = complete_data[a][4]
+        dzisiaj = dzisiaj.replace('.','-')
+        dzisiaj = dzisiaj[:-2] + '2023'
+        date_object = datetime.datetime.strptime(dzisiaj, '%d-%m-%Y').date()
+        date_object = date_object.strftime('%Y-%m-%d')
+        #print(date_object)
         #dzisiaj = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        sql = "INSERT INTO rankingi(web,cat,reg,fish,weight,place,bait,player,week,day,date) VALUES ( %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"        
-        val = (website, cat,reg, data[a],data[a+1], data[a+2],data[a+3], data[a+4],'nic', 'noc',date_object)
 
-          
+        if (complete_data[a][1]).find(" kg") > 0:
+            complete_data[a][1] = complete_data[a][1].replace(' kg','')
+            complete_data[a][1] = complete_data[a][1].replace(' ','')
+        else:
+            if (complete_data[a][1]).find(" g")   > 0:
+                complete_data[a][1] = complete_data[a][1].replace(' g','')
+                complete_data[a][1] = '0.'+ complete_data[a][1]
+        #print (a,website, cat,reg, complete_data[a][0],complete_data[a][1], complete_data[a][2],complete_data[a][5], complete_data[a][3],'no_data', 'no_data',complete_data[a][4])
+
+        sql = "INSERT INTO rankingi(web,cat,reg,fish,weight,place,bait,player,week,day,date) VALUES ( %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        val = (website, cat,reg, complete_data[a][0],complete_data[a][1], complete_data[a][2],complete_data[a][5], complete_data[a][3],'no_data', 'no_data',date_object)
+
+
         #val = ('test','test2')
         mycursor.execute(sql, val)
         mydb.commit()
